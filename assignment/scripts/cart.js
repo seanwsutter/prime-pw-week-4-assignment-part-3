317d9e8a8ae10db49fd25eea71a5bbfe96387bd6
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2021-05-29 21:08:06 -0500

    created two functions, added questions and notes

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!! 
+// We want to see how you are testing your code!!!
+let myCart = []; // I can change name to 'basket'
+
+function addToCart(item) {
+  // console.log('Added item to cart:', item);
+  myCart.push(item);
+  return true;
+  // return console.log(true);
+  /* QUESTION: required - 'return true indicating the item was added',
+  Is this asking to indicate/appear true in the console log (text is purple in console log)*/
+}  console.log('Testing function, addToCart...');
+addToCart('Eggs');
+addToCart('Bacon');
+addToCart('Cheese');
+addToCart('Coffee');
+// console.log('Listing items in myCart...');
+function listItems(array) {
+  for (let i=0; i<array.length; i++) {
+  console.log(array[i]);
+  }
+} console.log('testing function, listItems...');
+listItems(myCart);
+/* QUESTION: should I remove 'array' from function so its like this, and when I call it?
+    function listItems() {
+      for (let i=0; i<array.length; i++) {
+      console.log(array[i]);
+      }
+    } console.log('testing function, listItems...');
+    listItems();
+*/
